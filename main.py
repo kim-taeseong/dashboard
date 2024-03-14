2a9ab0721910312edc0bec47968c725471811bd7
taeseong <dnfrkwhr2424@naver.com>
2024-03-14 17:18:54 +0900

    feat: metric에 container, header 생성

--- main.py
+++ main.py
@@ -12,10 +12,13 @@ df = df[df['ITEMCODE'].isin(['90303', '90319'])]
 df_rh02 = df[df['TIMECODE'] == 'RH02']
 
 print(df_rh02)
+col1, col2 = st.columns(2)
+container1 = col1.container(border=True)
+container1.header('수도권')
 df_rh02_pb = df_rh02[df_rh02['ITEMCODE'] == '90303']
 delta = round(df_rh02_pb[df_rh02_pb['STATIONCODE'] == 1].iloc[-1]['VALUE'] - df_rh02_pb[df_rh02_pb['STATIONCODE'] == 1].iloc[-2]['VALUE'], 4)
-st.metric('납 측정수치', df_rh02_pb[df_rh02_pb['STATIONCODE'] == 1].iloc[-1]['VALUE'], f'{delta} ng/m\u00B3')
+container1.metric('납 측정수치', df_rh02_pb[df_rh02_pb['STATIONCODE'] == 1].iloc[-1]['VALUE'], f'{delta} ng/m\u00B3')
 
 df_rh02_ca = df_rh02[df_rh02['ITEMCODE'] == '90319']
 delta = round(df_rh02_ca[df_rh02_ca['STATIONCODE'] == 1].iloc[-1]['VALUE'] - df_rh02_ca[df_rh02_ca['STATIONCODE'] == 1].iloc[-2]['VALUE'], 4)
-st.metric('칼슘 측정수치', df_rh02_ca[df_rh02_ca['STATIONCODE'] == 1].iloc[-1]['VALUE'], f'{delta} ng/m\u00B3')
+container1.metric('칼슘 측정수치', df_rh02_ca[df_rh02_ca['STATIONCODE'] == 1].iloc[-1]['VALUE'], f'{delta} ng/m\u00B3')

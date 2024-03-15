5a988f183a9252ff95d3dd4f750377b8f3f49b2b
taeseong <dnfrkwhr2424@naver.com>
2024-03-15 16:25:03 +0900

    feat: 모든 station code에 대해 container, metric, page_link 생성

--- main.py
+++ main.py
@@ -9,9 +9,21 @@ from data_frame import make_data_frame
 result = get_api_data()
 df_rh02 = make_data_frame(result)
 print(df_rh02)
-col1, col2 = st.columns(2)
-container1 = col1.container(border=True)
+row1 = st.columns(3)
+row2 = st.columns(3)
+row3 = st.columns(3)
+row4 = st.columns(3)
 
 
-make_metric(df_rh02, container1, 1)
-container1.page_link('pages/station1.py', label='상세페이지', use_container_width=True)
+for i, col in enumerate(row1 + row2 + row3 + row4):
+    container = col.container(border=True)
+    make_metric(df_rh02, container, i+1)
+    container.page_link('pages/station1.py', label='상세페이지', use_container_width=True)
+# if 'station' not in st.session_state:
+#     st.session_state['station'] = None
+
+# def set_session(station):
+#     st.session_state['station'] = station
+
+# if container1.button('그래프', on_click=set_session(1), use_container_width=True):
+#     st.switch_page('pages/station1.py')
